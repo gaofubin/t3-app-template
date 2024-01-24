e9d603773e7d5572c9ad5193b61a6fffafa855a6
fubingao <451009913@qq.com>
2024-01-24 22:51:43 +0800

    update switch toggle

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -4,9 +4,7 @@ import { NavItems } from "@/components/constants/side-nav";
 
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/hooks/useSidebar";
-import { Separator } from "@/components/ui/separator";
-import { Button } from "@/components/ui/button";
-import { ChevronRight } from "lucide-react";
+import { BsArrowLeftShort } from "react-icons/bs";
 
 interface SidebarProps {
   className?: string;
@@ -24,12 +22,19 @@ export default function Sidebar({ className }: SidebarProps) {
   return (
     <nav
       className={cn(
-        `relative hidden h-screen border-r pt-16 md:block`,
+        `relative hidden h-screen border-r pt-20 md:block`,
         status && "duration-500",
         isOpen ? "w-72" : "w-[78px]",
         className
       )}
     >
+      <BsArrowLeftShort
+        className={cn(
+          "absolute -right-3 top-20 cursor-pointer rounded-full border bg-background text-3xl text-foreground",
+          !isOpen && "rotate-180"
+        )}
+        onClick={handleToggle}
+      />
       <div className="space-y-4 py-4">
         <div className="px-3 py-2">
           <div className="mt-3 space-y-1">
@@ -39,15 +44,6 @@ export default function Sidebar({ className }: SidebarProps) {
             />
           </div>
         </div>
-      </div>
-      <div className="mt-30 absolute bottom-5 w-full space-y-2 px-3">
-        <Separator />
-        <Button
-          onClick={handleToggle}
-          className={cn("h-10 w-full bg-foreground", isOpen && "rotate-180")}
-        >
-          <ChevronRight className="h-4 w-4" />
-        </Button>
       </div>
     </nav>
   );
